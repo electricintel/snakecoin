069afb84eed7ded082c8e45f8654cd71459dfde4
snakenest <182860650@qq.com>
2018-05-19 10:21:22 +0800

    snakecoin

---
+++ genesis.py
@@ -0,0 +1,6 @@
+from block import Block
+import datetime as date
+
+def create_genesis_block():
+    # Manually construct a block with index zero and arbitrary previous hash
+    return Block(0, date.datetime.now(), "Genesis Block", "0")
